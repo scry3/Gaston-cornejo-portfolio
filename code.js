2df5d4e6bc7098f13c4e57caa8451bc601bdccf3
scry3 <133330053+scry3@users.noreply.github.com>
2023-09-18 15:36:32 -0300

    Update code.js

--- code.js
+++ code.js
@@ -20,14 +20,15 @@ function efectoHabilidades(){
         habilidades[0].classList.add("javascript");
         habilidades[1].classList.add("html");
         habilidades[2].classList.add("css");
-        habilidades[3].classList.add("java");
-        habilidades[4].classList.add("sql");
-        habilidades[5].classList.add("inglesE");
-        habilidades[6].classList.add("inglesH");
-        habilidades[7].classList.add("inglesEs");
-        habilidades[8].classList.add("portuguesE");
-        habilidades[9].classList.add("portuguesH");
-        habilidades[10].classList.add("portuguesEs");
+        habilidades[3].classList.add("python");
+        habilidades[4].classList.add("java");
+        habilidades[5].classList.add("sql");
+        habilidades[6].classList.add("inglesE");
+        habilidades[7].classList.add("inglesH");
+        habilidades[8].classList.add("inglesEs");
+        habilidades[9].classList.add("portuguesE");
+        habilidades[10].classList.add("portuguesH");
+        habilidades[11].classList.add("portuguesEs");
     }
 }
 window.onscroll = function(){

7a1f5b9dfe2f8e3d682180a07d3d6d54b81274fd
scry3 <133330053+scry3@users.noreply.github.com>
2024-08-05 19:59:20 -0300

    Update code.js

--- code.js
+++ code.js
@@ -16,19 +16,20 @@ function efectoHabilidades(){
     var skills = document.getElementById("skills");
     var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
     if(distancia_skills >= 300){
-        let habilidades = document.getElementsByClassName("progreso");
-        habilidades[0].classList.add("javascript");
-        habilidades[1].classList.add("html");
-        habilidades[2].classList.add("css");
-        habilidades[3].classList.add("python");
-        habilidades[4].classList.add("java");
-        habilidades[5].classList.add("sql");
-        habilidades[6].classList.add("inglesE");
-        habilidades[7].classList.add("inglesH");
-        habilidades[8].classList.add("inglesEs");
-        habilidades[9].classList.add("portuguesE");
-        habilidades[10].classList.add("portuguesH");
-        habilidades[11].classList.add("portuguesEs");
+      let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("git");
+        habilidades[1].classList.add("javascript");
+        habilidades[2].classList.add("html");
+        habilidades[3].classList.add("css");
+        habilidades[4].classList.add("python");
+        habilidades[5].classList.add("java");
+        habilidades[6].classList.add("sql");
+        habilidades[7].classList.add("inglesE");
+        habilidades[8].classList.add("inglesH");
+        habilidades[9].classList.add("inglesEs");
+        habilidades[10].classList.add("portuguesE");
+        habilidades[11].classList.add("portuguesH");
+        habilidades[12].classList.add("portuguesEs");
     }
 }
 window.onscroll = function(){
